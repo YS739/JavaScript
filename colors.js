864618585fbab7d04ab9a718bb8d61e7c1929a90
ysj <suk921@gmail.com>
2022-10-01 18:13:58 +0900

    js

---
+++ colors.js
@@ -0,0 +1,32 @@
+var Llinks = {
+  setColor: function (color) {
+    var alist = document.querySelectorAll("a");
+    i = 0;
+    while (i < alist.length) {
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+  },
+};
+var body = {
+  setColor: function (color) {
+    document.querySelector("body").style.color = color;
+  },
+  setBackgroundColor: function (color) {
+    document.querySelector("body").style.backgroundColor = color;
+  },
+};
+function nightdayhandler(self) {
+  var target = document.querySelector("body");
+  if (self.value === "night") {
+    body.setBackgroundColor("black");
+    body.setColor("white");
+    self.value = "day";
+    Llinks.setColor("white");
+  } else {
+    body.setBackgroundColor("white");
+    body.setColor("black");
+    self.value = "night";
+    Llinks.setColor("blue");
+  }
+}
